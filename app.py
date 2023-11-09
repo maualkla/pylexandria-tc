0aaed569e3f120f703da3a4780053e6bf6ca93e8
Mauricio Alcala <mauricio.alcala.4@gmail.com>
2023-11-09 23:27:34 +0100

    feature -> service delete added

--- app.py
+++ app.py
@@ -831,89 +831,171 @@ def workspace():
 @app.route('/transaction', methods=['GET'])
 def transaction():
     try:
-        ## Validate the required authentication headers are present
-        if request.headers.get('SessionId') and request.headers.get('TokenId'):
-            ## In case are present, call validate session. True if valid, else not valid. Fixed to true
-            _auth = validateSession(request.headers.get('SessionId'), request.headers.get('TokenId'))
-            ## If validateSession return false, delete the session id.
-            if _auth == False: deleteSession(request.headers.get('SessionId'))
-        else: 
-            ## Fixed to true to allow outside calls to log in to the system,
-            _auth = False
-        if _auth:
-            ## list all the values to be returned in the get object.
-            _trx_fields = ['dateTime','id','userId','alert','action','severity'] 
-            ### Set the base for the json block to be returned. Define the data index for the list of trxs
-            _json_data_block = {"items": []}
-            ## Define _limit, _count, containsData and query
-            ## If query filter present in url params it will save it, else will set False.
-            _query = False if 'filter' not in request.args else request.args.get('filter')
-            ## If id filter present in url params it will save it, else will set false.
-            _id = False if 'id' not in request.args else request.args.get('id')
-            ## set default value for limit and count. 
-            _limit =  10 
-            _count = 0
-            _action = False
-            _alert = "N"
-            _userId = False
-            ## Validate if _query present
-            if _query:
-                ## calls to splitParams sending the _query form the request. If query correct returns a 
-                ## dictionary with the params as key value.
-                _parameters = Helpers.splitParams(_query)
-                ## if limit param present set the limit value
-                _limit = int(_parameters['limit']) if 'limit' in _parameters else _limit
-                ## if username param present, set the owner param
-                _action = str(_parameters['action']) if 'action' in _parameters else _action
-                ## if shortCode param present, set the shortCode param
-                _userId = str(_parameters['userId']) if 'userId' in _parameters else _userId
-                if 'alert' in _parameters:
-                    _alert = True if str(_parameters['alert']).lower() == 'true' else False
+        ## Method: POST /workspace
+        if request.method == 'POST':
+            ## Validate the required authentication headers are present
+            if request.headers.get('SessionId') and request.headers.get('TokenId'):
+                ## In case are present, call validate session. True if valid, else not valid. Fixed to true
+                _auth = validateSession(request.headers.get('SessionId'), request.headers.get('TokenId'))
+                ## If validateSession return false, delete the session id.
+                if _auth == False: deleteSession(request.headers.get('SessionId'))
+            else: 
+                ## Fixed to true to allow outside calls to log in to the system,
+                _auth = False
+            if _auth:
+                ## list all the values to be returned in the get object.
+                _trx_fields = ['dateTime','id','userId','alert','action','severity'] 
+                ### Set the base for the json block to be returned. Define the data index for the list of trxs
+                _json_data_block = {"items": []}
+                ## Define _limit, _count, containsData and query
+                ## If query filter present in url params it will save it, else will set False.
+                _query = False if 'filter' not in request.args else request.args.get('filter')
+                ## If id filter present in url params it will save it, else will set false.
+                _id = False if 'id' not in request.args else request.args.get('id')
+                ## set default value for limit and count. 
+                _limit =  10 
+                _count = 0
+                _action = False
+                _alert = "N"
+                _userId = False
+                ## Validate if _query present
+                if _query:
+                    ## calls to splitParams sending the _query form the request. If query correct returns a 
+                    ## dictionary with the params as key value.
+                    _parameters = Helpers.splitParams(_query)
+                    ## if limit param present set the limit value
+                    _limit = int(_parameters['limit']) if 'limit' in _parameters else _limit
+                    ## if username param present, set the owner param
+                    _action = str(_parameters['action']) if 'action' in _parameters else _action
+                    ## if shortCode param present, set the shortCode param
+                    _userId = str(_parameters['userId']) if 'userId' in _parameters else _userId
+                    if 'alert' in _parameters:
+                        _alert = True if str(_parameters['alert']).lower() == 'true' else False
 
-            ## Validate the 4 possible combinations for the query of the users search
-            if _id:
-                ## The case of id is present will search for that specific email
-                _search = trx_ref.where(filter=FieldFilter("id", "==", _id))
-            elif _action: 
-                ## the case of shortCode is present wull search for it.
-                _search = trx_ref.where(filter=FieldFilter("action", "==", _action))
-            elif _alert != "N":
-                ## The case username is present, will search with the specific username. 
-                _search = trx_ref.where(filter=FieldFilter("alert", "==", _alert))
-            elif _userId:
-                ## In case activate is present, will search for active or inactive users.
-                _search = trx_ref.where(filter=FieldFilter("userId", "==", _userId))
+                ## Validate the 4 possible combinations for the query of the users search
+                if _id:
+                    ## The case of id is present will search for that specific email
+                    _search = trx_ref.where(filter=FieldFilter("id", "==", _id))
+                elif _action: 
+                    ## the case of shortCode is present wull search for it.
+                    _search = trx_ref.where(filter=FieldFilter("action", "==", _action))
+                elif _alert != "N":
+                    ## The case username is present, will search with the specific username. 
+                    _search = trx_ref.where(filter=FieldFilter("alert", "==", _alert))
+                elif _userId:
+                    ## In case activate is present, will search for active or inactive users.
+                    _search = trx_ref.where(filter=FieldFilter("userId", "==", _userId))
+                else:
+                    ## In case any param was present, will search all
+                    _search = trx_ref
+                ## Loop in all the trxs inside the trx_ref object
+                for _trx in _search.stream():
+                    ## set the temporal json_blocl
+                    _json_block_l = {}
+                    ## apply the to_dict() to the current trx to use their information.
+                    _acc = _trx.to_dict()
+                    ## Add a +1 to the count
+                    _count += 1
+                    ## Iterates into the _trx_fields object to generate the json object for that user.
+                    for _x in _trx_fields:
+                        ## Generates the json object.
+                        _json_block_l[_x] = _acc[_x]
+                    ## Each iteration, append the trx block to the main payload.
+                    _json_data_block["items"].append(_json_block_l)
+                    if _count+1 > _limit: break
+                ## Before return a response, adding parameters for the get.
+                _json_data_block["limit"] = _limit
+                _json_data_block["count"] = _count
+                ## In case count > 0 it returns True, else False.
+                _json_data_block["containsData"] = True if _count > 0 else False 
+                _json_data_block["query"] = _query
+                return jsonify(_json_data_block), 200
             else:
-                ## In case any param was present, will search all
-                _search = trx_ref
-            ## Loop in all the trxs inside the trx_ref object
-            for _trx in _search.stream():
-                ## set the temporal json_blocl
-                _json_block_l = {}
-                ## apply the to_dict() to the current trx to use their information.
-                _acc = _trx.to_dict()
-                ## Add a +1 to the count
-                _count += 1
-                ## Iterates into the _trx_fields object to generate the json object for that user.
-                for _x in _trx_fields:
-                    ## Generates the json object.
-                    _json_block_l[_x] = _acc[_x]
-                ## Each iteration, append the trx block to the main payload.
-                _json_data_block["items"].append(_json_block_l)
-                if _count+1 > _limit: break
-            ## Before return a response, adding parameters for the get.
-            _json_data_block["limit"] = _limit
-            _json_data_block["count"] = _count
-            ## In case count > 0 it returns True, else False.
-            _json_data_block["containsData"] = True if _count > 0 else False 
-            _json_data_block["query"] = _query
-            return jsonify(_json_data_block), 200
-        else:
-            ## Missing authorization headers.
-            return jsonify({"status": "Error", "code": 401, "reason": "Invalid Authorization"}), 401
+                ## Missing authorization headers.
+                return jsonify({"status": "Error", "code": 401, "reason": "Invalid Authorization"}), 401
+        ## Method: DELETE /workspace
+        elif request.method == 'DELETE':
+            _errors = 0
+            ## Validate the required authentication headers are present
+            if request.headers.get('SessionId') and request.headers.get('TokenId'):
+                ## In case are present, call validate session. True if valid, else not valid. Fixed to true
+                _auth = validateSession(request.headers.get('SessionId'), request.headers.get('TokenId'))
+                ## If validateSession return false, delete the session id.
+                if _auth == False: deleteSession(request.headers.get('SessionId'))
+            else: 
+                ## Fixed to true to allow outside calls to log in to the system,
+                _auth = False
+            if _auth:
+                ## Logic to get params ######################################################
+                ## If query filter present in url params it will save it, else will set False.
+                _query = False if 'filter' not in request.args else request.args.get('filter')
+                ## If id filter present in url params it will save it, else will set false.
+                _id = False if 'id' not in request.args else request.args.get('id')
+                _owner = False
+                _shortCode = False
+                _active = "N"
+
+                ## Validate if _query present
+                if _query:
+                    ## calls to splitParams sending the _query form the request. If query correct returns a 
+                    ## dictionary with the params as key value.
+                    _parameters = Helpers.splitParams(_query)
+                    ## if username param present, set the owner param
+                    _owner = str(_parameters['owner']) if 'owner' in _parameters else _owner
+                    ## if shortCode param present, set the shortCode param
+                    _shortCode = str(_parameters['shortCode']) if 'shortCode' in _parameters else _shortCode
+                    ## if active param present validates the str value, if true seet True, else set False. if not present, 
+                    ## sets _active to "N" to ignore the value
+                    if 'active' in _parameters:
+                        _active = True if str(_parameters['active']).lower() == 'true' else False
+                        
+                ## Logic to get data
+                ## Validate the 4 possible combinations for the query of the users search
+                if _id:
+                    ## The case of id is present will search for that specific email
+                    _search = wsp_ref.where(filter=FieldFilter("TaxId", "==", _id))
+                elif _shortCode: 
+                    ## the case of shortCode is present wull search for it.
+                    _search = wsp_ref.where(filter=FieldFilter("ShortCode", "==", _shortCode))
+                    if _active != "N":
+                        ## In case the _active param is present in valid fashion, will search for active or inactiv
+                        ## e users.
+                        _search = _search.where(filter=FieldFilter("Active", "==", _active))
+                elif _owner:
+                    ## The case username is present, will search with the specific username. 
+                    _search = wsp_ref.where(filter=FieldFilter("Active", "==", _owner))
+                    if _active != "N":
+                        ## In case the _active param is present in valid fashion, will search for active or inactiv
+                        ## e users.
+                        _search = _search.where(filter=FieldFilter("Active", "==", _active))
+                else:
+                    ## In case any param was present, will search all
+                    _search = wsp_ref.where(filter=FieldFilter("TaxId", "==", ""))
+
+                ## Loop in all the users inside the users_ref object
+                _trx = {}
+                for _us in _search.stream():
+                    ## apply the to_dict() to the current user to use their information.
+                    _acc = _us.to_dict()
+                    ## validate if deletion was successful
+                    if deleteWorkspace(_acc['TaxId']):
+                        ## Add the trx number to the user email to the return response
+                        _trx[_acc['TaxId']] = transactionPost(_auth['userId'], False, 2, "Workspace Delete")
+                    else:
+                        ## Sums error count
+                        _errors += 1
+                ## validated the numer of errors
+                if _errors == 0:
+                    ## if no errors returns only the trx 
+                    return jsonify(_trx), 200
+                else:
+                    ## if errors, returns the error count and the trx successful
+                    return jsonify({"status": "Error", "code": 500, "reason": "There was errors while deleting", "errorCount": _errors, "transactions": [_trx]}), 401
+            else:
+                ## Missing authorization headers.
+                return jsonify({"status": "Error", "code": 401, "reason": "Invalid Authorization"}), 401
     except Exception as e:
         return jsonify({"status": "Error", "code": str(e)[0:3], "reason": str(e)}), 500
-
 ## (deprecated) v0.01 Auth a token.
 @app.route('/vauth', methods=['POST'])
 def vauth():
